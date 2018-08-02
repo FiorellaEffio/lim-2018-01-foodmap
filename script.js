aa98da2389d73d2954a418db82ed2c134031518f
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-08-02 10:28:58 -0500

    inicializando mapa sin boton

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-// var preload = document.getElementById("preload");
-// var loading = 0;
-// var id = setInterval(frame, 64);
-//
-// function frame(){
-//   if(loading == 100) {
-//    clearInterval(id);
-//    window.open("welcome.html", "_self");
-//   } else {
-//      loading = loading + 1;
-//      if(loading == 90) {
-//        preload.style.animation = "fadeout 1s ease";
-//      }
-//   }
-// }
+var preload = document.getElementById("preload");
+var loading = 0;
+var id = setInterval(frame, 64);
+
+function frame(){
+  if(loading == 100) {
+   clearInterval(id);
+   window.open("welcome.html", "_self");
+  } else {
+     loading = loading + 1;
+     if(loading == 90) {
+       preload.style.animation = "fadeout 1s ease";
+     }
+  }
+}
 //inicializar mapa
 var map;
 function initMap(lat, lng) {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat, lng},
-    zoom: 8
+    zoom: 13
   });
+  marker = new google.maps.Marker({
+    map:map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: {lat, lng}
+  });
+  marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
 //geolocalizacion
 function geoFindMe() {
@@ -39,3 +54,4 @@ function geoFindMe() {
   output.innerHTML = "<p>Locating…</p>";
   navigator.geolocation.getCurrentPosition(success, error);
 }
+geoFindMe();
