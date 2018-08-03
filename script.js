b0bc72090f565f4b8fdeba2316705d8041e14751
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-08-03 11:17:10 -0500

    hmmm feos estilos

--- script.js
+++ script.js
@@ -1,43 +1,116 @@
-//inicializar mapa
-var map;
-function initMap(lat, lng) {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat, lng},
-    zoom: 16
-  });
-  marker = new google.maps.Marker({
-    map:map,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    position: {lat, lng}
-  });
-  marker.addListener('click', toggleBounce);
+let map, infowindow;
+//coordenadas de laboratoria en barranco
+let laboratoriaLocation = {
+  lat : -12.145437,
+  lng : -77.021891
 }
+let currentLocation;
+const success = (position) => {
+  console.log(position);
+  currentLocation = {
+    lat : position.coords.latitude,
+    lng : position.coords.longitude
+  };
+  initMap();
+};
+const error = () => {
+  alert("Te mostraremos restaurantes cerca a Laboratoria :)");
+  currentLocation = laboratoriaLocation;
+  initMap();
+};
 
-function toggleBounce() {
+const toggleBounce = () => {
   if (marker.getAnimation() !== null) {
     marker.setAnimation(null);
   } else {
     marker.setAnimation(google.maps.Animation.BOUNCE);
   }
 }
-//geolocalizacion
-function geoFindMe() {
-  var output = document.getElementById("out");
-  if (!navigator.geolocation){
-    console.log("<p>Geolocation is not supported by your browser</p>");
-    return;
-  }
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    initMap(latitude, longitude);
-  };
-  function error() {
-    console.log("Unable to retrieve your location");
-    initMap(-12.145437, -77.021891);
-  };
-  output.innerHTML = "<p>Cargando…</p>";
-  navigator.geolocation.getCurrentPosition(success, error);
+function initMap() {
+  console.log(currentLocation);
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: currentLocation,
+    zoom: 16
+  });
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: currentLocation,
+    radius: 500,
+    type: ['restaurant']
+  }, callback);
 }
-geoFindMe();
+
+function callback(results, status) {
+  console.log(results);
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+      printRestaurantsDivs(results[i]);
+    }
+  }
+}
+function printRestaurantsDivs(objRestaurant) {
+  let answerOpenNow ='No hay información :(';
+  console.log(objRestaurant.hasOwnProperty('opening_hours'));
+  if(objRestaurant.hasOwnProperty('opening_hours') === true){
+    if(objRestaurant.opening_hours.open_now === false){
+      answerOpenNow = 'NO :(';
+    } else {
+      answerOpenNow = 'SI :)';
+    }
+  }
+  let restaurantContainer = document.getElementById('restaurants');
+  restaurantContainer.innerHTML += `
+  <div class="divRestaurant card">
+    <div class="card-body">
+    <h1>${objRestaurant.name}</h1>
+    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target=${'#R'+objRestaurant.id}>Mas info</button>
+    </div>
+  </div>`;
+  let modalForRestaurantContainer = document.getElementById('modalsForRestaurants');
+  photoRestaurant = ''
+  modalsForRestaurants.innerHTML += `
+  <div class="modal fade" id=${'R'+objRestaurant.id} role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">${objRestaurant.name}</h4>
+          <img src=""/>
+        </div>
+        <div class="modal-body">
+          <div id=${'M'+objRestaurant.id}></div>
+          <p>Ubicado en : ${objRestaurant.vicinity}</p>
+          <p>Abierto ahora: ${answerOpenNow}</p>
+          <p>Calificación: ${objRestaurant.rating}</p>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+navigator.geolocation.getCurrentPosition(success, error);
--- splash.js
+++ splash.js
@@ -1,6 +1,6 @@
 var preload = document.getElementById("preload");
 var loading = 0;
-var id = setInterval(frame, 64);
+var id = setInterval(frame, 15);
 function frame(){
   if(loading == 100) {
    clearInterval(id);
@@ -8,7 +8,7 @@ function frame(){
   } else {
      loading = loading + 1;
      if(loading == 90) {
-       preload.style.animation = "fadeout 1s ease";
+       preload.style.animation = "fadeout 1.5s ease";
      }
   }
 }
